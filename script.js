ba06d9f72a7d67347f839a9bb87f50c6332faf2b
Ryan Smith <78234726+ryan1000000@users.noreply.github.com>
2022-05-28 13:08:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13313,14 +13313,4 @@ function danceTiles(tiles) {
   })
 }
 
-const shareButton = document.querySelector('.share-button');
-const shareDialog = document.querySelector('.share-dialog');
-const closeButton = document.querySelector('.close-button');
 
-shareButton.addEventListener('click', event => {
-  shareDialog.classList.add('is-open');
-});
-
-closeButton.addEventListener('click', event => {
-  shareDialog.classList.remove('is-open');
-});
